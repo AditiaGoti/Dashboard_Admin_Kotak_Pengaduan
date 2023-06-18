6e504be96811fe8e191f953a35cfd0274b6b6434
Aditia Goti Pratama <gotiaditia26@gmail.com>
2023-06-18 17:16:13 +0700

    [Fix] Graph dashboard

--- src/components/Cards/CardPageVisits.vue
+++ src/components/Cards/CardPageVisits.vue
@@ -66,8 +66,8 @@ export default {
     'Akademik',
     'Perlengkapan/Fasilitas',
     'Kemahasiswaan',
-    'Sistem Informasi',
-    'Lain lain'
+    'Keuangan',
+    'Lain-lain'
   ];
 
   const data = labels.map((label) => {
@@ -90,8 +90,70 @@ export default {
         },
       ],
     },
-    // ...options
-  };
+    options: {
+          maintainAspectRatio: false,
+          responsive: true,
+          title: {
+            display: false,
+            text: "Orders Chart",
+          },
+          tooltips: {
+            mode: "index",
+            intersect: false,
+          },
+          hover: {
+            mode: "nearest",
+            intersect: true,
+          },
+          legend: {
+            labels: {
+              fontColor: "rgba(0,0,0,.4)",
+            },
+            align: "end",
+            position: "bottom",
+          },
+          scales: {
+            xAxes: [
+              {
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: "Kategori Pengaduan",
+                },
+                gridLines: {
+                  borderDash: [2],
+                  borderDashOffset: [2],
+                  color: "rgba(33, 50, 41, 0.3)",
+                  zeroLineColor: "rgba(33, 37, 41, 0.3)",
+                  zeroLineBorderDash: [2],
+                  zeroLineBorderDashOffset: [2],
+                },
+              },
+            ],
+            yAxes: [
+              {
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: "Total Pengaduan",
+                },
+                gridLines: {
+                  borderDash: [2],
+                  drawBorder: false,
+                  borderDashOffset: [2],
+                  color: "rgba(33, 37, 41, 0.2)",
+                  zeroLineColor: "rgba(33, 37, 41, 0.15)",
+                  zeroLineBorderDash: [2],
+                  zeroLineBorderDashOffset: [2],
+                },
+                ticks: {
+              beginAtZero: true, // Start y-axis from 0
+              precision: 0, // Display only regular numbers without decimal places
+            },
+              },
+            ],
+          },
+        },  };
 
   let ctx = document.getElementById("bar-chart-categories").getContext("2d");
   window.myBar = new Chart(ctx, config);
